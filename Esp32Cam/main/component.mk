07707a494fa75093cd61f9213f258b880296441b
BanhTriKiet <banhtrikiet1@gmail.com>
2024-06-09 20:24:42 +0700

    hello

--- Esp32Cam/main/component.mk
+++
@@ -1,7 +0,0 @@
-#
-# "main" pseudo-component makefile.
-#
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-COMPONENT_EMBED_TXTFILES := ../cert/AmazonRootCA1.pem
-COMPONENT_EMBED_TXTFILES += ../cert/certificate.pem.crt
-COMPONENT_EMBED_TXTFILES += ../cert/private.pem.key
